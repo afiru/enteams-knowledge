2225578fba507888ec8587e9887aa90f91bb7b9c
Naoki Soga <n.soga@sunplacom>
2022-08-12 17:26:59 +0900

    タイトル部分変更

--- wp-content/themes/WebPTemplatebyMarcat/include_files/single/singleTopics.php
+++ wp-content/themes/WebPTemplatebyMarcat/include_files/single/singleTopics.php
@@ -1,8 +1,9 @@
 <div class="wapper display_flex_stretch display_row base1Column topicsWap">
+    <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+    <?php get_template_part('include_files/single/main/mainTitle'); ?>
     <main class="main1Column topicsSingleMain">
-        <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-            <?php get_template_part('include_files/single/main/mainSingleTopics'); ?>
-        <?php endwhile; // end of the loop. ?>
+        <?php get_template_part('include_files/single/main/mainSingleTopics'); ?>
     </main>
+    <?php endwhile; // end of the loop. ?>
 </div>
 
